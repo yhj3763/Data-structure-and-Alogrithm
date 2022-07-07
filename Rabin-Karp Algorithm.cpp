1ddde829e1e56face8fb764641998e33bbedfe2b
yhj3763 <65998859+yhj3763@users.noreply.github.com>
2022-07-06 23:08:01 -0700

    Update Rabin-Karp Algorithm.cpp

--- Rabin-Karp Algorithm.cpp
+++ Rabin-Karp Algorithm.cpp
@@ -3,7 +3,9 @@
 //If target has 5 characters like apple in string, it will be
 //a*2^4 + p*2^3 + p*2^2 + l*2^1 + e*2^0
 //Therefore, we can have different sum for different characters
-//When we want to move to next character, we need to use sum = 2 * (sum - originalstring[charachter we want to remove] * power(usually 2^size of target-1)+originalstring[character we want to add])
+//When we want to move to next character, we need to use 
+//sum = 2 * (sum - originalstring[charachter we want to remove] * power(usually 2^size of target-1)+originalstring[character we want to add])
+
 class Solution {
 public:
     int strStr(string haystack, string needle) {
