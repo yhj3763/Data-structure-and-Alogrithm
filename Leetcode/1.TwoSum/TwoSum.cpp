2635572a82af7b0e6ec261c390270e9f7370cb45
yhj3763 <65998859+yhj3763@users.noreply.github.com>
2023-01-26 21:01:36 +0900

    Update TwoSum.cpp

--- Leetcode/1.TwoSum/TwoSum.cpp
+++ Leetcode/1.TwoSum/TwoSum.cpp
@@ -1,3 +1,4 @@
+//for문을 중첩한 풀이
 public:
 vector<int> twoSum(vector<int>& nums, int target) {
       
@@ -13,4 +14,26 @@ vector<int> twoSum(vector<int>& nums, int target) {
    }
 
    return ans;
+}
+
+//자료구조 unordered_map을 이용한 풀이
+public:
+vector<int> twoSum(vector<int>& nums, int target) {
+      
+    unordered_map<int, int> num_map;
+    vector<int> ans;
+ 
+    for(int i = 0; i<nums.size();i++){
+        num_map[nums[i]] = i;
+    }
+
+    for(int i = 0; i<nums.size();i++){
+        if(num_map.find(target-nums[i])!= num_map.end()&& i!= num_map[target-nums[i]]){
+            ans.push_back(i);
+            ans.push_back(num_map[target-nums[i]]);
+            return ans;
+        }
+    }
+
+    return ans;
 }
